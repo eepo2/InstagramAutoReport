bf716d27c95710d1bf8ae23f6b36e5bf676e3dd7
Astro <100206062+xAstroDev@users.noreply.github.com>
2022-06-18 03:09:33 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,6 +18,6 @@ pip install colorama
 pip install pyfiglet
 printf "\e[0m\e[1;91m [\e[1;97m~\e[1;91m]\e[1;93m Running Scripts...\e[0m\n"
 sleep 2
-python3 main.py
+python3 reports.py
 
 printf "\e[0m\n"
